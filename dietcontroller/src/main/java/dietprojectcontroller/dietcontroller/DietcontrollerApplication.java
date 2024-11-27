1a0e761addeadedc5a44c466dd8565a5bb9cc9c0
DamLun <damianlunkiewicz@gmail.com>
2024-11-27 14:30:57 +0100

    First commit

---
+++ dietcontroller/src/main/java/dietprojectcontroller/dietcontroller/DietcontrollerApplication.java
@@ -0,0 +1,13 @@
+package dietprojectcontroller.dietcontroller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DietcontrollerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DietcontrollerApplication.class, args);
+	}
+
+}
---
+++ dietcontroller/src/test/java/dietprojectcontroller/dietcontroller/DietcontrollerApplicationTests.java
@@ -0,0 +1,13 @@
+package dietprojectcontroller.dietcontroller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DietcontrollerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
